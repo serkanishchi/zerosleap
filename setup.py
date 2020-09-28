80dd585fa8fed5c4e0f7632326689e7bb370817d
Serkan ISCI <serkanishchi@gmail.com>
2020-09-28 18:14:40 -0400

    Update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='zerosleap',
     version='0.0.1',
-    packages=['tests', 'tests.gui', 'tests.comp', 'tests.conn', 'tests.video', 'tests.processing', 'zerosleap',
+    packages=['tests', 'zerosleap',
               'zerosleap.gui', 'zerosleap.comp', 'zerosleap.conn', 'zerosleap.video', 'zerosleap.processing',
               'zerosleap.processing.tracking'],
     url='',
